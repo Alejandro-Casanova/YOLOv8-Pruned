7b6f29fb640433a9cb970fded56b6443c5f7fb5f
Alejandro Casanova <alejandro.casanova@disroot.org>
2024-08-26 17:50:57 +0900

    prepared trash detection

---
+++ test_runs_trash.sh
@@ -0,0 +1,5 @@
+: ' pruning_perf_change 33 (trash dataset) 
+Sparsity learn for 200 epochs, prune 50% and retrain for 200 epochs
+# 
+'
+python yolov8_pruning.py --cfg-file=my_config_files/trash_config.yaml
