942700bb3238c9347d986ff2467e2d931e3d6cbc
casanova <alejandro.casanova.martin@alumnos.upm.es>
2025-05-02 12:36:25 +0200

    refactor: clean up

---
+++ Plotter.py
@@ -0,0 +1,166 @@
+import argparse
+import glob
+import json
+import logging
+import os
+import pprint
+import sys
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+class Plotter:
+
+    def __init__(self):
+        self._currentPlot = 0 # Plot index corresponding to current run
+        self._out_dir = "prune_results"
+        if not os.path.exists(self._out_dir):
+            os.makedirs(self._out_dir)
+        self._filename = f"{self._out_dir}/pruning_perf_change"
+
+        # Find next free index to save plot
+        while glob.glob('{}_{:d}.*'.format(self._filename, self._currentPlot)):
+            self._currentPlot += 1
+        
+        # Append index to filename
+        self._filename = '{}_{:d}'.format(self._filename, self._currentPlot)
+
+        self._logger = logging.getLogger(__name__)
+
+    def append_dict_to_log(
+        self, 
+        dict: dict = {},
+        description: str = "" 
+    ):
+        # Save the console args passed to script
+        with open('{}.txt'.format(self._filename), 'a') as f:
+            #f.write(json.dumps(dict))
+            f.write("\n" + description + "\n")
+            pprint.pprint(dict, f)
+
+    def save_pruning_performance_graph(
+        self,
+        x, y1, y2, y3,
+        subTitleStr: str = "",
+        save_results_json: bool = True
+    ) -> None: 
+        """
+        Draw performance change graph
+        Each call will overwrite the graph corresponding to the current run. On the next run, the graph index will be incremented.
+        Also saves plot data as json file.
+
+        Parameters
+        ----------
+        x : List
+            Parameter numbers of all pruning steps
+        y1 : List
+            mAPs after fine-tuning of all pruning steps
+        y2 : List
+            FLOPs of all pruning steps
+        y3 : List
+            mAPs after pruning (not fine-tuned) of all pruning steps
+
+        Returns
+        -------
+
+        """
+        try:
+            plt.style.use("ggplot")
+        except:
+            pass
+
+        x, y1, y2, y3 = np.array(x), np.array(y1), np.array(y2), np.array(y3)
+        y2_ratio = y2 / y2[0]
+
+        # create the figure and the axis object
+        fig, ax = plt.subplots(figsize=(8, 6))
+
+        # plot the pruned mAP and recovered mAP
+        ax.set_xlabel('Tamaño del model (1 - ratio de poda) %')
+        ax.set_ylabel('mAP')
+        ax.plot(x, y1, label='mAP tras re-entrenar')
+        ax.scatter(x, y1)
+        ax.plot(x, y3, color='tab:gray', label='mAP tras podar')
+        ax.scatter(x, y3, color='tab:gray')
+
+        # create a second axis that shares the same x-axis
+        ax2 = ax.twinx()
+
+        # plot the second set of data
+        ax2.set_ylabel('FLOPs')
+        ax2.plot(x, y2_ratio, color='tab:orange', label='FLOPs')
+        ax2.scatter(x, y2_ratio, color='tab:orange')
+
+        # add a legend
+        lines, labels = ax.get_legend_handles_labels()
+        lines2, labels2 = ax2.get_legend_handles_labels()
+        ax2.legend(lines + lines2, labels + labels2, loc='best')
+
+        # set plot limits
+        ax.set_xlim(105, -5)
+        ax.set_ylim(0, max(y1) + 0.05)
+        ax2.set_ylim(0.05, 1.05)
+
+        # calculate the highest and lowest points for each set of data
+        max_y1_idx = np.argmax(y1)
+        min_y1_idx = np.argmin(y1)
+        max_y2_idx = np.argmax(y2)
+        min_y2_idx = np.argmin(y2)
+        max_y1 = y1[max_y1_idx]
+        min_y1 = y1[min_y1_idx]
+        max_y2 = y2_ratio[max_y2_idx]
+        min_y2 = y2_ratio[min_y2_idx]
+
+        # add text for the highest and lowest values near the points
+        ax.text(x[max_y1_idx], max_y1 - 0.05, f'max mAP = {100.0*max_y1:.2f}%', fontsize=10)
+        ax.text(x[min_y1_idx], min_y1 + 0.02, f'min mAP = {100.0*min_y1:.2f}%', fontsize=10)
+        ax2.text(x[max_y2_idx], max_y2 - 0.05, f'max FLOPs = {max_y2 * y2[0] / 1e9:.2f}G', fontsize=10)
+        ax2.text(x[min_y2_idx], min_y2 + 0.02, f'min FLOPs = {min_y2 * y2[0] / 1e9:.2f}G', fontsize=10)
+
+        plt.title('Comparación del mAP y los FLOPs con respecto al ratio de poda')
+        plt.suptitle(subTitleStr)
+        
+        # Write/overwrite the plot corresponding to current run
+        plt.savefig('{}.png'.format(self._filename))
+
+        if not save_results_json:
+            return
+        
+        # Save plot data as json file too
+        dict_to_save = {
+            "x": x.tolist(),
+            "y1": y1.tolist(),
+            "y2": y2.tolist(),
+            "y3": y3.tolist()
+        }
+
+        with open('{}.json'.format(self._filename), "w") as fp:
+            json.dump(dict_to_save , fp, indent=4) 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Pruning Plotter")
+
+    # Default parameters from Ultralytics Yolo config file
+    parser.add_argument('--results-json', type=str, required=True,
+                        help='Pruning results json file')
+    parser.add_argument('--subtitle', type=str, default="", 
+                        help='Plot subtitle string.')
+    
+    args = parser.parse_args()
+    
+    json_dict = None
+
+    with open(args.results_json, "r") as fp:
+        json_dict = json.load(fp) 
+
+    if json_dict is None:
+        sys.exit(1)
+    
+    plotter = Plotter()
+
+    plotter.save_pruning_performance_graph(
+        json_dict['x'], json_dict['y1'], json_dict['y2'], json_dict['y3'],
+        args.subtitle, False
+    )
+
+    sys.exit(0)
--- plot_prune_graph.py
+++ plot_prune_graph.py
@@ -3,59 +3,68 @@ import matplotlib.pyplot as plt
 import json
 import glob
 import os
+import argparse
 
-# Step 1: Define the directory containing the JSON files
-directory_path = '/home/alex/Desktop/ultralytics/selected_prune_results'
+if __name__ == "__main__":
 
-# Step 2: Get all .json files in the directory
-json_files = glob.glob(os.path.join(directory_path, '*.json'))
+    parser = argparse.ArgumentParser(description="Plot Prune Graph")
+    parser.add_argument('-dir', '--directory', type=str, default=".\\selected_prune_results", 
+                        help='Pretrained pruning target model file')
+    parser.add_argument('-y', '--y-axis', type=str, default='speedup',
+                        choices=['speedup', 'prune_ratio', 'accuracy', 'acc_drop'],
+                        help='Set the logging level')
+    
+    args = parser.parse_args()
 
-# Step 3: Initialize a list to hold the dictionaries
-data = []
+    # Step 1: Define the directory containing the JSON files
+    directory_path = args.directory
 
-# Step 4: Read each JSON file and extract the dictionaries
-for json_file in json_files:
-    with open(json_file, 'r') as file:
-        # Load the JSON data
-        json_data = json.load(file)
-        # Assuming each json_data is a dictionary with 'x' and 'y' keys
-        data.append(json_data)
+    # Step 2: Get all .json files in the directory
+    json_files = glob.glob(os.path.join(directory_path, '*.json'))
 
-# Step 5: Create a DataFrame from the list of dictionaries
-df = pd.DataFrame(data)
-df['prune-ratio'] = df['x'].apply(lambda x: x[0] - x[1])
-df['acc_drop'] = df['y1'].apply(lambda x: (x[1] - x[0])*100.0)
-df['acc'] = df['y1'].apply(lambda x: x[1] * 100.0)
-df['speedup'] = df['y2'].apply(lambda x: x[0] / x[1])
+    # Step 3: Initialize a list to hold the dictionaries
+    data = []
 
-# Step 6: Print the DataFrame (optional)
-print(df)
+    # Step 4: Read each JSON file and extract the dictionaries
+    for json_file in json_files:
+        with open(json_file, 'r') as file:
+            # Load the JSON data
+            json_data = json.load(file)
+            # Assuming each json_data is a dictionary with 'x' and 'y' keys
+            data.append(json_data)
 
-Y_VAR_TO_PLOT = 'speedup'
-Y_LABEL = "Speed Up"
+    # Step 5: Create a DataFrame from the list of dictionaries
+    df = pd.DataFrame(data)
+    df['prune_ratio'] = df['x'].apply(lambda x: x[0] - x[1])
+    df['acc_drop'] = df['y1'].apply(lambda x: (x[1] - x[0])*100.0)
+    df['accuracy'] = df['y1'].apply(lambda x: x[1] * 100.0)
+    df['speedup'] = df['y2'].apply(lambda x: x[0] / x[1])
 
-# Step 7: Plot the points
-plt.figure(figsize=(10, 6))
-scatter = plt.scatter(df['prune-ratio'], df[Y_VAR_TO_PLOT], alpha=0.7, cmap='viridis', edgecolors='w', linewidth=2.5)
-# scatter = plt.scatter(df['prune-ratio'], df['acc'])
+    # Step 6: Print the DataFrame (optional)
+    print(df)
 
-# Setp 8: Style
+    # Step 7: Plot the points
+    plt.figure(figsize=(10, 6))
+    scatter = plt.scatter(df['prune_ratio'], df[args.y_axis], alpha=0.7, cmap='viridis', edgecolors='w', linewidth=2.5)
+    # scatter = plt.scatter(df['prune_ratio'], df['accuracy'])
 
-# # Add a colorbar
-# plt.colorbar(scatter, label='Color Scale')
+    # Step 8: Style
 
-# Title and Axis Labels
-plt.title('Prune Results', fontsize=16, fontweight='bold')
-plt.xlabel('Prune Ratio', fontsize=14)
-plt.ylabel(Y_LABEL, fontsize=14)
+    # # Add a colorbar
+    # plt.colorbar(scatter, label='Color Scale')
 
-# Add gridlines
-plt.grid(True, linestyle='--', alpha=0.7)
+    # Title and Axis Labels
+    plt.title('Prune Results', fontsize=16, fontweight='bold')
+    plt.xlabel('Prune Ratio', fontsize=14)
+    plt.ylabel(args.y_axis, fontsize=14)
 
-# Add annotations for each point
-for i in range(len(df)):
-    plt.annotate(f'({df["prune-ratio"][i]:.2f}, {df[Y_VAR_TO_PLOT][i]:.2f})', (df['prune-ratio'][i], df[Y_VAR_TO_PLOT][i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize=8)
+    # Add grid lines
+    plt.grid(True, linestyle='--', alpha=0.7)
 
-# Show the plot
-plt.tight_layout()
-plt.show()
+    # Add annotations for each point
+    for i in range(len(df)):
+        plt.annotate(f'({df["prune_ratio"][i]:.2f}, {df[args.y_axis][i]:.2f})', (df['prune_ratio'][i], df[args.y_axis][i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize=8)
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
