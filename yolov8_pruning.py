484253335941c142583c3fd3ee72fd41a757a24b
casanova <alejandro.casanova.martin@alumnos.upm.es>
2025-06-21 21:03:11 +0200

    refactor: move c2v_v2 to separate module

--- yolov8_pruning.py
+++ yolov8_pruning.py
@@ -34,77 +34,7 @@ from ultralytics.cfg import TASK2DATA
 import logging
 from Plotter import Plotter
 import torch_pruning as tp
-
-def infer_shortcut(bottleneck):
-    c1 = bottleneck.cv1.conv.in_channels
-    c2 = bottleneck.cv2.conv.out_channels
-    return c1 == c2 and hasattr(bottleneck, 'add') and bottleneck.add
-
-# Quick fix to emulate layer "split"
-class C2f_v2(nn.Module):
-    # CSP Bottleneck with 2 convolutions
-    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        self.c = int(c2 * e)  # hidden channels
-        self.cv0 = Conv(c1, self.c, 1, 1)
-        self.cv1 = Conv(c1, self.c, 1, 1)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
-
-    def forward(self, x):
-        # y = list(self.cv1(x).chunk(2, 1))
-        y = [self.cv0(x), self.cv1(x)]
-        y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(torch.cat(y, 1))
-
-
-def transfer_weights(c2f, c2f_v2):
-    c2f_v2.cv2 = c2f.cv2
-    c2f_v2.m = c2f.m
-
-    state_dict = c2f.state_dict()
-    state_dict_v2 = c2f_v2.state_dict()
-
-    # Transfer cv1 weights from C2f to cv0 and cv1 in C2f_v2
-    old_weight = state_dict['cv1.conv.weight']
-    half_channels = old_weight.shape[0] // 2
-    state_dict_v2['cv0.conv.weight'] = old_weight[:half_channels]
-    state_dict_v2['cv1.conv.weight'] = old_weight[half_channels:]
-
-    # Transfer cv1 batchnorm weights and buffers from C2f to cv0 and cv1 in C2f_v2
-    for bn_key in ['weight', 'bias', 'running_mean', 'running_var']:
-        old_bn = state_dict[f'cv1.bn.{bn_key}']
-        state_dict_v2[f'cv0.bn.{bn_key}'] = old_bn[:half_channels]
-        state_dict_v2[f'cv1.bn.{bn_key}'] = old_bn[half_channels:]
-
-    # Transfer remaining weights and buffers
-    for key in state_dict:
-        if not key.startswith('cv1.'):
-            state_dict_v2[key] = state_dict[key]
-
-    # Transfer all non-method attributes
-    for attr_name in dir(c2f):
-        attr_value = getattr(c2f, attr_name)
-        if not callable(attr_value) and '_' not in attr_name:
-            setattr(c2f_v2, attr_name, attr_value)
-
-    c2f_v2.load_state_dict(state_dict_v2)
-
-
-def replace_c2f_with_c2f_v2(module):
-    for name, child_module in module.named_children():
-        if isinstance(child_module, C2f):
-            # Replace C2f with C2f_v2 while preserving its parameters
-            shortcut = infer_shortcut(child_module.m[0])
-            c2f_v2 = C2f_v2(child_module.cv1.conv.in_channels, child_module.cv2.conv.out_channels,
-                            n=len(child_module.m), shortcut=shortcut,
-                            g=child_module.m[0].cv2.conv.groups,
-                            e=child_module.c / child_module.cv2.conv.out_channels)
-            transfer_weights(child_module, c2f_v2)
-            setattr(module, name, c2f_v2)
-        else:
-            replace_c2f_with_c2f_v2(child_module)
-
+from yolov8_c2v_patch import replace_c2f_with_c2f_v2
 
 def save_model_v2(self: BaseTrainer):
     """
@@ -615,9 +545,9 @@ def prune(args, plotter: Plotter):
     LOGGER.info(f"Before Pruning: FLOPs={base_flops / 1e9: .5f} G, #Params={base_num_params / 1e6: .5f} M, mAP={init_map: .5f}")
 
     # prune same ratio of filter based on initial size
-    pruning_ratio = 1 - math.pow((1 - args.target_prune_rate), 1 / args.iterative_steps)
+    pruning_ratio = 1 - math.pow((1 - args.target_prune_rate), 1 / args.iterative_steps) if args.iterative_steps > 0 else 1
     LOGGER.info(f"PRUNE RATIO: {pruning_ratio}")
-    fine_steps = round(100.0 / pruning_ratio) # Steps taken inside torch-pruning to approach desired prune ratio at give global step
+    fine_steps = round(100.0 / pruning_ratio) if pruning_ratio != 0 else 1 # Steps taken inside torch-pruning to approach desired prune ratio at give global step
     LOGGER.info(f"SMOOTH STEPS: {fine_steps}")
     #pruning_ratio = args.target_prune_rate # FOR TESTING: should cut model by half twice if iterative_steps is 2
     
