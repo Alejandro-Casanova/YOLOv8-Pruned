9328711d439f2b36adf4dfa48284bd2b3faf7dc7
casanova <alejandro.casanova.martin@alumnos.upm.es>
2025-06-22 21:56:03 +0200

    feat: update model export script to include dynamic image size handling

---
+++ cleanup_models.bat
@@ -0,0 +1,1 @@
+del /Q my_models\*.onnx
--- export_models.bat
+++ export_models.bat
@@ -2,58 +2,23 @@
 setlocal enabledelayedexpansion
 
 REM List of model suffixes
-set models=10 20 30 50 60 70 80 90
+set models=00 10 20 30 50 60 70 80 90
+set image_sizes=160 320 480 640
 
-REM Iterate over each model and call export_model.py
+REM Iterate over each model and image size, export both normal and quantized models
 for %%X in (%models%) do (
-    echo Exporting model best_%%X.pt...
-    python .\export_model.py -m .\my_models\best_%%X.pt -i 320 -f onnx --optimize
+    for %%S in (%image_sizes%) do (
+        REM Export normal model
+        echo Exporting model best_%%X.pt with image size %%S...
+        python .\export_model.py -m .\my_models\best_%%X.pt -i %%S -f onnx --optimize
+        set "new_name=best_%%X_imgsz%%S.onnx"
+        ren .\my_models\best_%%X.onnx !new_name!
 
-    REM Rename the exported ONNX file to add imgsz suffix
-    set "new_name=best_%%X_imgsz320.onnx"
-    ren .\my_models\best_%%X.onnx !new_name!
+        REM Export quantized model
+        echo Exporting model best_%%X.pt with image size %%S and 8 bit quantization...
+        python .\export_model.py -m .\my_models\best_%%X.pt -i %%S -f onnx --optimize -q
+    )
 )
-
-REM Do the same but apply 16 bit quantization
-for %%X in (%models%) do (
-    echo Exporting model best_%%X.pt with 16 bit quantization...
-    python .\export_model.py -m .\my_models\best_%%X.pt -i 320 -f onnx --optimize -q16
-
-    REM Rename the exported ONNX file to add imgsz suffix
-    set "new_name=best_%%X_imgsz320_quant16.onnx"
-    ren .\my_models\best_%%X.onnx !new_name!
-)
-
-REM Do the same but apply 8 bit quantization
-for %%X in (%models%) do (
-    echo Exporting model best_%%X.pt with 8 bit quantization...
-    python .\export_model.py -m .\my_models\best_%%X.pt -i 320 -f onnx --optimize -q8
-
-    REM Rename the exported ONNX file to add imgsz suffix
-    set "new_name=best_%%X_imgsz320_quant8.onnx"
-    ren .\my_models\best_%%X.onnx !new_name!
-)
-
-REM Do the same but for the 640 image size
-for %%X in (%models%) do (
-    echo Exporting model best_%%X.pt with image size 640...
-    python .\export_model.py -m .\my_models\best_%%X.pt -i 640 -f onnx --optimize
-
-    REM Rename the exported ONNX file to add imgsz suffix
-    set "new_name=best_%%X_imgsz640.onnx"
-    ren .\my_models\best_%%X.onnx !new_name!
-)
-
-REM Do the same for the unpruned model trash_yolov8n_pretrained.pt
-echo Exporting unpruned model trash_yolov8n_pretrained.pt...
-python .\export_model.py -m .\my_models\trash_yolov8n_pretrained.pt -i 320 -f onnx --optimize
-ren .\my_models\trash_yolov8n_pretrained.onnx trash_yolov8n_pretrained_imgsz320.onnx
-python .\export_model.py -m .\my_models\trash_yolov8n_pretrained.pt -i 640 -f onnx --optimize
-ren .\my_models\trash_yolov8n_pretrained.onnx trash_yolov8n_pretrained_imgsz640.onnx
-python .\export_model.py -m .\my_models\trash_yolov8n_pretrained.pt -i 320 -f onnx --optimize -q16
-ren .\my_models\trash_yolov8n_pretrained.onnx trash_yolov8n_pretrained_imgsz320_quant16.onnx
-python .\export_model.py -m .\my_models\trash_yolov8n_pretrained.pt -i 320 -f onnx --optimize -q8
-ren .\my_models\trash_yolov8n_pretrained.onnx trash_yolov8n_pretrained_imgsz320_quant8.onnx
 
 echo All models exported.
 pause
