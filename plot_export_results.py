1901ef067f260ce69782d1b6feb7661f03c824ec
casanova <alejandro.casanova.martin@alumnos.upm.es>
2025-06-22 21:55:32 +0200

    feat: add script for plotting export results with FPS and mAP50-95 metrics

---
+++ plot_export_results.py
@@ -0,0 +1,43 @@
+import os
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set the base directory
+base_dir = "my_models/export_results"
+
+# Collect data
+results = {}
+for class_name in os.listdir(base_dir):
+    class_dir = os.path.join(base_dir, class_name)
+    if not os.path.isdir(class_dir):
+        continue
+    results[class_name] = {}
+    for fname in os.listdir(class_dir):
+        if fname.endswith('.json'):
+            prune_rate = fname.replace('.json', '')
+            with open(os.path.join(class_dir, fname), 'r') as f:
+                data = json.load(f)
+            results[class_name][prune_rate] = data
+
+# Prepare colormap
+class_names = list(results.keys())
+num_classes = len(class_names)
+cmap = plt.get_cmap('Blues')
+colors = [cmap(i / (num_classes + 1)) for i in range(1, num_classes + 1)]
+
+# Plotting
+for idx, (class_name, prunes) in enumerate(results.items()):
+    prune_rates = []
+    accuracies = []
+    for prune_rate, data in sorted(prunes.items(), key=lambda x: float(x[0])):
+        prune_rates.append(data.get('FPS', 0))
+        accuracies.append(data.get('mAP50-95', 0))
+    plt.plot(prune_rates, accuracies, marker='o', label=class_name, color=colors[idx])
+
+plt.xlabel('FPS')
+plt.ylabel('mAP50-95')
+plt.title('mAP50-95 vs FPS')
+plt.legend()
+plt.grid(True)
+plt.show()
