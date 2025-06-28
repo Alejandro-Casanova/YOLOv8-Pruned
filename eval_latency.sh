0eb2e629a8e926ea3d575d8c8a12a391716b7bd1
Alejandro Casanova <alejandro.casanova.martin@alumnos.upm.es>
2025-06-28 14:15:11 +0200

    chore: add results from inference in raspi

--- eval_latency.sh
+++ eval_latency.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Iterate over all .onnx files in my_models folder
-for f in my_models/*.onnx; do
-    echo "Processing model: $f"
-    for seed in {1..5}; do
-        echo "  Using seed: $seed"
-        python ./test_val_model.py -m "$f" -s "$seed"
+# Iterate over seeds first, then over all .onnx files in my_models folder
+for seed in $(seq 1 5); do
+    echo "Using seed: $seed"
+    for f in my_models/*.onnx; do
+        echo "  Processing model: $f"
+        python ./eval_latency.py -m "$f" -s "$seed"
     done
 done
